ce369bdf7bb4fc01e5b675585dcb3d9b5a09921e
azat-khatyn <khalitova.yana@gmail.com>
2020-05-24 12:04:38 +0200

    comments added

--- rest_api_covid.py
+++ rest_api_covid.py
@@ -11,15 +11,15 @@ def confirmed_cases_estimation():
         days = int(request.args.get('days'))
         if days < 0:
             return 'No covid data is available prior to the 1st of March 2020!'
+
         date_of_prediction = (date(2020, 3, 1) + timedelta(days=days)).isoformat().replace('-0', '-', 2)
 
+        # reading covid data and filtering by the input date
         covid_data = pd.read_csv('covid_data.csv')
         covid_data_on_date_of_prediction = covid_data[covid_data['date'] == date_of_prediction].drop(['date'], axis=1)
 
         if covid_data_on_date_of_prediction.shape[0] == 0:
             return 'No covid data is available yet for the date %s' % date_of_prediction
-
-        k_nearest_neighbors_to_nk = pd.read_csv('nearest_countries_to_nk.csv')
 
         countries_mapping = {
             'Myanmar': 'Burma',
@@ -30,12 +30,16 @@ def confirmed_cases_estimation():
             'Cambodia': 'Cambodia'
         }
 
+        # reading nearest countries data, mapping country names, and removing those that don't exist in covid data
+        k_nearest_neighbors_to_nk = pd.read_csv('nearest_countries_to_nk.csv')
         k_nearest_neighbors_to_nk_mapped_names = \
             pd.DataFrame(k_nearest_neighbors_to_nk.apply(lambda row: countries_mapping[row['name']], axis=1), columns=['country'])
         k_nearest_neighbors_to_nk_mapped_names = k_nearest_neighbors_to_nk_mapped_names[k_nearest_neighbors_to_nk_mapped_names['country'] != '']
 
+        # joining nearest countries DF and covid data DF
         nearest_countries_on_date_of_prediction = pd.merge(k_nearest_neighbors_to_nk_mapped_names, covid_data_on_date_of_prediction, on='country')
 
+        # taking average number of confirmed cases among nearest countries
         confirmed = int(round(nearest_countries_on_date_of_prediction['confirmed'].mean()))
         return 'On the date %s the estimated number of confirmed cases in North Korea is %d' % (date_of_prediction, confirmed)
     except Exception as exc:
