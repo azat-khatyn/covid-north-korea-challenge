5065ccbc1e4eb256734e4415a817dc56aaa11de7
azat-khatyn <khalitova.yana@gmail.com>
2020-05-24 11:25:55 +0200

    generating date in the service

--- rest_api_covid.py
+++ rest_api_covid.py
@@ -1,7 +1,14 @@
 from flask import Flask
+from flask import request
+from datetime import date, timedelta
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, Yana!'
-
+@app.route('/confirmed_cases_estimation', methods=['GET'])
+def confirmed_cases_estimation():
+    try:
+        days = int(request.args.get('days'))
+        date_of_prediction = date(2020, 3, 1) + timedelta(days=days)
+        return date_of_prediction.isoformat()
+    except Exception as exc:
+        return 'Internal error: %s' % exc
